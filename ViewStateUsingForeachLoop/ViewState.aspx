0b1548c0965dc5d0f61837752e665fd4d8fc65ee
emonkhan129 <emonkhan129@yahoo.com>
2016-10-06 11:13:04 +0600

    hi

---
+++ ViewStateUsingForeachLoop/ViewState.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewState.aspx.cs" Inherits="ViewStateUsingForeachLoop.ViewState" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <br/>
+        Name<asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox><br/>
+        <asp:Button ID="saveButton" runat="server" Text="Save" OnClick="saveButton_Click" />
+        <asp:Button ID="showAllButton" runat="server" Text="Show All" OnClick="showAllButton_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ViewStateUsingForeachLoop/ViewState.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewStateUsingForeachLoop
+{
+    public partial class ViewState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+             string name = nameTextBox.Text;
+
+            if (ViewState["student"] == null)
+            {
+                List<string> studentList = new List<string>();
+                studentList.Add(name);
+                ViewState["student"] = studentList;
+            }
+            else
+            {
+                List<string> studentList = (List<string>) ViewState["student"];
+                studentList.Add(name);
+                ViewState["student"] = studentList;
+            }
+
+            nameTextBox.Text = string.Empty;
+        }
+
+        protected void showAllButton_Click(object sender, EventArgs e)
+        {
+            List<string> studenList = (List<string>)ViewState["student"];
+            foreach (string allStudents in studenList)
+            {
+                Response.Write(allStudents+"<br>");
+            }
+        }
+    }
+}
